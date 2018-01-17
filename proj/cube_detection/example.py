a5696981da3c8321eee570d7298bde294e97f80f
Unknown <1198995013@qq.com>
2018-01-17 19:12:42 +0800

    Clean up cv demo

--- proj/cube_detection/example.py
+++ proj/cube_detection/example.py
@@ -7,7 +7,7 @@ import cv2
 colorLower = np.array([0, 100, 100], dtype="uint8")
 colorUpper = np.array([255, 200, 200], dtype="uint8")
 
-#Hue from http://color.yafla.com/
+# Hue from http://color.yafla.com/
 lower_yellow = np.array([30, 60, 60])
 upper_yellow = np.array([40, 255, 255])
 
@@ -19,21 +19,22 @@ while True:
     (grabbed, frame) = camera.read(cv2.COLOR_BGR2HSV)
     cv2.imshow('original', frame)
 
+    # Remove noise
+    frame = cv2.GaussianBlur(frame, (3, 3), 0)
+    frame = cv2.bilateralFilter(frame, 9, 75, 75)
+
+    # Detect yellow specific area
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
     res = cv2.bitwise_and(frame, frame, mask=mask)
-    cv2.imshow('frame', frame)
     cv2.imshow('mask', mask)
-    cv2.imshow('res', res)
-    frame = mask
 
-    frame = cv2.GaussianBlur(frame, (3, 3), 0)
-
-    (img, cnts, _) = cv2.findContours(frame.copy(), cv2.RETR_EXTERNAL,
+    (img, cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_LIST,
                                       cv2.CHAIN_APPROX_SIMPLE)
     if len(cnts) > 0:
         # Detect the largest region
         cnt = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+        approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
         rect = np.int32(cv2.boxPoints(cv2.minAreaRect(cnt)))
         # Draw a rectangular frame around the detected object
         frame = cv2.drawContours(frame, [rect], -1, (100, 100, 256), 4)
